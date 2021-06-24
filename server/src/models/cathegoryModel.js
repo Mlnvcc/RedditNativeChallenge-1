fb64c2ce236d319c91de104aea158eb9e0726959
Alina <mlnvccc@gmail.com>
2021-06-24 19:01:05 +0300

    updated

--- server/src/models/cathegoryModel.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const cathegorySchema = new Schema({
-  title: String,
-});
-
-const Cathegory = model('Cathegory', cathegorySchema);
-
-module.exports = Cathegory;
