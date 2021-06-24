f065cd6a10b8e39c4689a598bbd8939fec4f8b36
Alina <mlnvccc@gmail.com>
2021-06-24 17:51:11 +0300

    Post model + seed

---
+++ server/src/models/postModel.js
@@ -0,0 +1,29 @@
+const mongoose = require ('mongoose')
+
+const postSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId], 
+    ref: 'User'
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User'
+  },
+  content: {
+    type: String,
+    // required: true,
+    },
+  date: {
+   type: Date,
+  },
+})
+
+module.exports = mongoose.model("Post", postSchema)
