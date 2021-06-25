b25ea26a447fa3c84b8f4c46d2e266682c399b03
Vladislav <faustus15@mail.ru>
2021-06-25 15:05:09 +0300

    15:05

--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { View } from "react-native";
 import Header from "../Header/Header";
 import LowerMenu from "../LowerMenu/LowerMenu";
-import PostList from "../PostList/PostList"
+import PostList from "../PostList/PostList";
 
 export default function MainPage() {
   return (
