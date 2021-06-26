59d714b2716fdd163b4f572add8902de5c62db93
Alina <mlnvccc@gmail.com>
2021-06-26 15:04:11 +0300

    do not push

--- client/src/components/PostList/PostList.js
+++ client/src/components/PostList/PostList.js
@@ -1,5 +1,15 @@
 import React, { useEffect } from "react";
-import { StyleSheet, View, TextInput, Button, Text } from "react-native";
+import {  
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Alert,
+  ScrollView,
+  FlatList,
+  Button 
+} from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import Item from "../Item";
 import { getContent } from "../../redux/actions/content";
@@ -12,13 +22,119 @@ export default function PostList() {
   const posts = useSelector(state => state.content);
 
   return (
-    <>
+    <View style={styles.container}>
       <Text>Последние посты</Text>
-      <View>
+      <FlatList style={styles.list}>
+      <View style={styles.separator}>
         {posts.map(el => {
           return <Item key={el._id} el={el} />;
         })}
       </View>
-    </>
+        </FlatList>
+      </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    marginTop:20,
+  },
+  list: {
+    paddingHorizontal: 17,
+    backgroundColor:"#E6E6E6",
+  },
+  separator: {
+    marginTop: 10,
+  },
+  /******** card **************/
+  card:{
+    shadowColor: '#00000021',
+    shadowOffset: {
+      width: 2
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    marginVertical: 8,
+    backgroundColor:"white"
+  },
+  cardHeader: {
+    paddingVertical: 17,
+    paddingHorizontal: 16,
+    borderTopLeftRadius: 1,
+    borderTopRightRadius: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  cardContent: {
+    paddingVertical: 12.5,
+    paddingHorizontal: 16,
+  },
+  cardFooter:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 12.5,
+    paddingBottom: 25,
+    paddingHorizontal: 16,
+    borderBottomLeftRadius: 1,
+    borderBottomRightRadius: 1,
+    backgroundColor:"#EEEEEE",
+  },
+  cardImage:{
+    flex: 1,
+    height: 150,
+    width: null,
+  },
+  /******** card components **************/
+  title:{
+    fontSize:18,
+    flex:1,
+  }, 
+  description:{
+    fontSize:15,
+    color:"#888",
+    flex:1,
+    marginTop:5,
+    marginBottom:5,
+  },
+  time:{
+    fontSize:13,
+    color: "#808080",
+    marginTop: 5
+  },
+  icon: {
+    width:25,
+    height:25,
+  },
+  iconData:{
+    width:15,
+    height:15,
+    marginTop:5,
+    marginRight:5
+  },
+  timeContainer:{
+    flexDirection:'row'
+  },
+  /******** social bar ******************/
+  socialBarContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    flex: 1
+  },
+  socialBarSection: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    flex: 1,
+  },
+  socialBarlabel: {
+    marginLeft: 8,
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+  },
+  socialBarButton:{
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});  
--- server/src/models/commentModel.js
+++ server/src/models/commentModel.js
@@ -1,4 +1,3 @@
-
 const { Schema, model } = require('mongoose');
 
 const commentSchema = new Schema({
