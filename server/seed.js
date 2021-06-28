f70f72184a52d6ed24e13608f30f2c2e67682e48
Milyukovich <milyukovartur@gmail.com>
2021-06-28 09:36:03 +0300

    Fix bugs add posts

--- server/seed.js
+++ server/seed.js
@@ -1,46 +1,45 @@
 const { disconnect, connect } = require('./src/db/connect');
 const mongoose = require('mongoose');
-const moment = require('moment')
-const Post = require('./src/models/postModel')
+const moment = require('moment');
+const Post = require('./src/models/postModel');
 
 const posts = [
- { 
-title: "Elbrus" ,
-description: "sjbkjsndkcjkjdc kd ck dcknw d",
-date: moment().subtract(6, 'days').calendar(),
-},
-{ 
-title: "Elbrus" ,
-description: "sjbkjsndkcjkjdc kd ck dcknw d",
-date: moment().subtract(6, 'days').calendar(),
-},
-{ 
-title: "Elbrus" ,
-description: "sjbkjsndkcjkjdc kd ck dcknw d",
-date: moment().subtract(6, 'days').calendar(),
-},
-{ 
-title: "Elbrus" ,
-description: "sjbkjsndkcjkjdc kd ck dcknw d",
-date: moment().subtract(6, 'days').calendar(),
-},
-{ 
-title: "Elbrus" ,
-description: "sjbkjsndkcjkjdc kd ck dcknw d",
-date: moment().subtract(6, 'days').calendar(),
-},
-{
-title: "Elbrus" ,
-description: "sjbkjsndkcjkjdc kd ck dcknw d",
-date: moment().subtract(6, 'days').calendar(),
-}
-]
-
+  {
+    title: 'Elbrus',
+    description: 'sjbkjsndkcjkjdc kd ck dcknw d',
+    date: moment().subtract(6, 'days').calendar(),
+  },
+  {
+    title: 'Elbrus',
+    description: 'sjbkjsndkcjkjdc kd ck dcknw d',
+    date: moment().subtract(6, 'days').calendar(),
+  },
+  {
+    title: 'Elbrus',
+    description: 'sjbkjsndkcjkjdc kd ck dcknw d',
+    date: moment().subtract(6, 'days').calendar(),
+  },
+  {
+    title: 'Elbrus',
+    description: 'sjbkjsndkcjkjdc kd ck dcknw d',
+    date: moment().subtract(6, 'days').calendar(),
+  },
+  {
+    title: 'Elbrus',
+    description: 'sjbkjsndkcjkjdc kd ck dcknw d',
+    date: moment().subtract(6, 'days').calendar(),
+  },
+  {
+    title: 'Elbrus',
+    description: 'sjbkjsndkcjkjdc kd ck dcknw d',
+    date: moment().subtract(6, 'days').calendar(),
+  },
+];
 
 const seed = async () => {
-  connect()
+  connect();
 
-  await Promise.all(posts.map(el => Post.create(el)))
+  await Promise.all(posts.map(el => Post.create(el)));
 
   disconnect();
 };
