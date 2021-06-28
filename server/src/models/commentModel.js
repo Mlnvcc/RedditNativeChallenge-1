051f20145f1fd2ecd181a6855a0ac926bcdeddf0
Roman <rrlroww@gmail.com>
2021-06-28 18:22:55 +0300

    asd

--- server/src/models/commentModel.js
+++ server/src/models/commentModel.js
@@ -7,7 +7,7 @@ const commentSchema = new Schema({
     type: String,
   },
   creatorLogin: String,
-  comments: [{ type: mongoose.Schema.Types.ObjectId }],
+  comments: [{ type: mongoose.Schema.Types.ObjectId,ref:"Comment" }],
   creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 
   likes: [],
